dfd9da20b0ab57e57094a4c1a3be6b8e21f7a5cd
AyoMoses1 <ayomoses111@gmail.com>
2023-11-14 21:39:10 +0100

    Work on server performance chart

--- src/pages/dashboard/analytics/components/SystemPerformance.tsx
+++ src/pages/dashboard/analytics/components/SystemPerformance.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import { ChartOptions, DoughnutControllerChartOptions } from "chart.js";
 
-const SystemPerformance = () => {
-  return (
-    <div>SystemPerformance</div>
-  )
-}
+type SystemPerformanceProps = {
+  // Your component props if any
+};
 
-export default SystemPerformance
+const SystemPerformance: React.FC<SystemPerformanceProps> = () => {
+  const data = {
+    labels: ["Used Space"],
+    datasets: [
+      {
+        label: "Facility Status",
+        data: [70, 40],
+        fill: false,
+        backgroundColor: ["#62C28D", "#E3EBE2"],
+        hoverOffset: 4,
+        tension: 0.1,
+      },
+    ],
+  };
+
+  const options: Partial<
+    ChartOptions<"doughnut"> & DoughnutControllerChartOptions
+  > = {
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        display: false, // Hide the y-axis
+      },
+    },
+    plugins: {
+      legend: {
+        display: true,
+        position: "bottom",
+        labels: {
+          boxWidth: 20,
+          color: "black",
+        },
+      },
+    },
+  };
+
+  return <Doughnut data={data} height={400} options={options} />;
+};
+
+export default SystemPerformance;
