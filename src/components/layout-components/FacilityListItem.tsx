572cd0530faf6333569168a7b9043d35f158e9b3
AyoMoses1 <ayomoses111@gmail.com>
2023-11-29 15:29:23 +0100

    Fix facilities nav bug

--- src/components/layout-components/FacilityListItem.tsx
+++ src/components/layout-components/FacilityListItem.tsx
@@ -1,16 +1,37 @@
-import { HStack, Text } from "@chakra-ui/react"
-import React from "react"
+import { HStack, Text } from "@chakra-ui/react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { TEXT_DARK_GRAY } from "../../utils/color";
-import { Link } from "react-router-dom";
 import { getSlug } from "../../utils/helpers";
 
-interface FacilityListItemProps extends FacilityData { }
-const FacilityListItem: React.FC<FacilityListItemProps> = ({name}) => {
+interface FacilityListItemProps extends FacilityData {}
+const FacilityListItem: React.FC<FacilityListItemProps> = ({ name }) => {
+  const navigate = useNavigate();
+  const handleClick = (e) => {
+    e.preventDefault();
+    navigate(`/dashboard/facilities/${getSlug(name!)}`);
+  };
   return (
-    <HStack rounded={"sm"} to={`/dashboard/facilities/${getSlug(name!)}`} _hover={{ bg: "gray.100" }} color={TEXT_DARK_GRAY }  spacing={2} as={Link} alignItems={"center"} p={3} h={"40px"}>
-      <Text textTransform={"capitalize"} color={"inherit"} fontWeight={"500"} fontSize={"sm"}>{name}</Text>
+    <HStack
+      rounded={"sm"}
+      _hover={{ bg: "gray.100" }}
+      color={TEXT_DARK_GRAY}
+      spacing={2}
+      onClick={handleClick}
+      alignItems={"center"}
+      p={3}
+      h={"40px"}
+    >
+      <Text
+        textTransform={"capitalize"}
+        color={"inherit"}
+        fontWeight={"500"}
+        fontSize={"sm"}
+      >
+        {name}
+      </Text>
     </HStack>
-  )
-}
+  );
+};
 
-export default FacilityListItem
+export default FacilityListItem;
